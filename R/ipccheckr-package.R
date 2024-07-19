651894c4c693a1d64c5fbb26d40c479402ea30e9
tomaszaba <tomas.zaba@outlook.com>
2024-07-19 22:25:36 +0200

    update

--- R/ipccheckr-package.R
+++ R/ipccheckr-package.R
@@ -3,22 +3,13 @@
 
 ## usethis namespace: start
 #' @importFrom dplyr across case_when group_by mutate n n_distinct rename summarise
-#' @importFrom dplyr ends_with
-#' @importFrom dplyr everything
-#' @importFrom dplyr filter
+#' @importFrom dplyr ends_with everything filter
 #' @importFrom lubridate ymd
-#' @importFrom nipnTK ageRatioTest digitPreference sexRatioTest skewKurt
-#' @importFrom nipnTK greensIndex
-#' @importFrom rlang .data
-#' @importFrom rlang quo
-#' @importFrom rlang quo_is_null
-#' @importFrom rlang sym
+#' @importFrom nipnTK ageRatioTest digitPreference sexRatioTest skewKurt greensIndex
+#' @importFrom rlang .data sym
 #' @importFrom scales label_percent label_pvalue
-#' @importFrom srvyr as_survey_design
-#' @importFrom srvyr survey_mean
-#' @importFrom stats na.omit prop.test sd
-#' @importFrom stats pnorm
-#' @importFrom stats setNames
+#' @importFrom srvyr as_survey_design survey_mean
+#' @importFrom stats na.omit prop.test sd pnorm setNames
 #' @importFrom zscorer addWGSR
 ## usethis namespace: end
 NULL
